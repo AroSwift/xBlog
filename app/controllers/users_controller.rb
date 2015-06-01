3215ac9d7d8218353249696f1bee326251e4f8c3
Aaron Barlow <abarlow505@gmail.com>
2015-06-01 15:49:50 -0400

    Working v.10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,22 @@ class UsersController < ApplicationController
   def create
     @username = params[:users][:username]
     @password = params[:users][:password]
-    if @username.empty? && @password.empty? then
 
 
-      flash[:key] = 'Your input can not be blank.'
+
+if @username.length < 8 then
+
+      flash[:error] = 'Your username must be at least 8 characters'
+      redirect_to :back
+    elsif @password.length < 8 then
+
+      flash[:error] = 'Your password must be at least 8 characters.'
       redirect_to :back
     else
-
       redirect_to :home
     end
   end
+
 
   def destroy
   end
