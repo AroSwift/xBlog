25e6810b087b3d1395e61c3ece5ce9b91714dd6a
Aaron Barlow <abarlow505@gmail.com>
2015-06-01 11:11:37 -0400

    Slow Progress on Login and Sign Up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   def new
-  	@username = Username.new
-  	@password = Password.new
-  end 
+  	@users = User.new
+  end
 
   def create
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
 
+    else
+      flash.now[:error] = 'Invalid username/password combination'
+      render 'new'
+    end
   end
 end
