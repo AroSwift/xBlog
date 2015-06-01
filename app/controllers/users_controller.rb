dfe5e652fb6b6d8bff95ea1f2fe08a2adc49bdf3
Aaron Barlow <abarlow505@gmail.com>
2015-06-01 16:22:18 -0400

    V0.11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,43 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    @password = Password.new(:password=>params[:password])
   end
 
   def create
     @username = params[:users][:username]
     @password = params[:users][:password]
+    @confirm_password = params[:users][:confirm_password]
 
 
 
-if @username.length < 8 then
+if @username.length < 5 then
 
-      flash[:error] = 'Your username must be at least 8 characters'
+      flash[:error] = 'Your username must be at least 5 characters'
+      redirect_to :back
+    elsif @username.length > 12 then
+
+      flash[:error] = 'Your username must not be greater than 12 characters'
       redirect_to :back
     elsif @password.length < 8 then
 
-      flash[:error] = 'Your password must be at least 8 characters.'
+      flash[:error] = 'Your password must be at least 8 characters'
       redirect_to :back
+    elsif @password.length > 20 then
+
+      flash[:error] = 'Your password must not be greater than 20 characters'
+      redirect_to :back
+     elsif @confirm_password.length != @password then
+
+      flash[:error] = "Your passwords don't match"
+      redirect_to :back 
+
     else
+      # DATABASE STUFF AFTER ALL CONDITIONS MEET
+
       redirect_to :home
     end
-  end
+end
 
 
   def destroy
