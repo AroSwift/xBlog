0dc88fbae674d7c3e9eec16af248119391a55029
Aaron Barlow <abarlow505@gmail.com>
2015-06-01 14:15:14 -0400

    Steady State

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = params[:users][:username]
     user = User.find_by(username: params[:user][:username].downcase)
     if user && user.authenticate(params[:user][:password])
       # Log the user in and redirect to the user's show page.
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
--- app/models/users.rb
+++ app/models/users.rb
@@ -2,12 +2,10 @@ class Users < ActiveRecord::Base
 
 	  attr_accessible :name, :password, :password_confirmation
 
- def change
+ def user
     create_table :users do |t|
       t.string :username
       t.string :password
- 
-      t.timestamps null: false
     end
   end
 
