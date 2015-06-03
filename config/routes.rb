8c9ac2861da24b4dc7c1b8e3d0e3881075044024
Aaron Barlow <abarlow505@gmail.com>
2015-06-03 16:08:11 -0400

    Fixed Post and Display Alpha v0.27

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root 'users#index'
 
   get     'post'    => 'posts#post'
   post     'post'    => 'posts#create'  
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get     'index'   => 'users#signup'
   get     'signup'  => 'users#signup'
   post    'signup'  => 'users#create'
-  get     'home'    => 'welcome#index'
+  get     'home'    => 'users#index'
 
   resources :users
   resources :sessions
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20150603190805) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
-    t.text     "post"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
