50980ab4e6f7623a5bce453c0d3daf2058722c4a
Aaron Barlow <abarlow505@gmail.com>
2015-06-15 15:27:49 -0400

    Partially functional edit user feature

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,8 +3,44 @@ class AdminController < ApplicationController
 	def create
 	end
 
+
+
 	def update
+
+		@username = params[:users][:pusername]
+    @password = params[:users][:ppassword]
+    @confirm_password = params[:users][:confirm_password]
+    @admin = params[:users][:padmin]
+    @id = params[:id]
+
+    user = User.find_by_id(@id)
+    user.username = @username
+    user.password = @password
+    user.admin = @admin
+    user.valid?
+
+    if user.errors.empty? then
+
+
+    	if @confirm_password == @password then
+
+
+      user.save(user_params)
+      flash[:error] = "The user '#{@username}' was updated"
+      redirect_to :admin_home
+
+    	else
+    		redirect_to admin_edit_users_path(:display => 'Your passwords do not match', :pusername => @username, :ppassword => @password, :padmin => @admin, :id => @id, :errors => user.errors.full_messages)
+    	end
+
+    else
+      flash[:error] = user.errors.full_messages
+      redirect_to admin_edit_users_path(:pusername => @username, :ppassword => @password, :padmin => @admin, :id => @id, :errors => user.errors.full_messages)
+    end
+
 	end
+
+
 
 	def logout
 		@_current_user = session[:current_user_id] = nil
@@ -32,5 +68,9 @@ class AdminController < ApplicationController
 			redirect_to admin_home_path(:display => "The user #{@dusername} was successfully deleted")
 		end
 	end
+
+	def user_params
+    params.require(:users).permit(:username, :password, :admin, :id)
+  end
 
 end
