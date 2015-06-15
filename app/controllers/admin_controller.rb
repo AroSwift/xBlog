e140017f0feffa910696c1a7770f9a7c5411b1a2
Aaron Barlow <abarlow505@gmail.com>
2015-06-15 14:31:27 -0400

    Added delete paramaters to delete links

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -18,9 +18,21 @@ class AdminController < ApplicationController
 		@dusername = params[:dusername]
 		@dpassword = params[:dpassword]
 		@dadmin = params[:dadmin]
+		@type = params[:dtype]
+		@author_id = params[:author_id]
+
 
 		User.where(:username => @dusername, :password => @dpassword, :admin => @dadmin).destroy_all
-		redirect_to home_path(:display => "The post #{@dusername} was successfully deleted")
+		redirect_to home_path(:display => "The user #{@dusername} was successfully deleted")
+
+		if @type == 'all' && @type != 'user' then
+			Post.where(author: @dusername, author_id: @author_id).each do |post|
+
+				Post.where(:title => @title, :author => @dusername, :author_id => @author_id).destroy_all
+				redirect_to home_path(:display => "The user #{@dusername} was successfully deleted")
+
+			end
+		end
 	end
 
 end
---
+++ spec/user_spec.rb
@@ -0,0 +1,15 @@
+Class Login
+end
+
+	before(:each) dp
+		@login = Login.new
+		@login.extend(Say)
+	end
+
+	describe User::login do
+		
+		it "takes user username and password"
+			@login.show.should == ""
+			experct(login).to eq 
+		end
+	end
