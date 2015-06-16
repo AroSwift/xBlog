48021772df1b0afcdf30fee80ad77e3d92c89a9e
Aaron Barlow <abarlow505@gmail.com>
2015-06-16 12:58:39 -0400

    Adding account functionality

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   put     'edit_post'           => 'posts#update'
   get     'edit_post'           => 'posts#edit_post'
 
-  get     'settings'             => 'account#settings'
+  get     'account'             => 'account#settings'
 
   get     'admin_home'          => 'admin#index'
   get     'admin_users'         => 'admin#users'
