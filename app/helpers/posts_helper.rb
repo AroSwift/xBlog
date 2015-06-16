91e21acd4bf813811dba8642b34efad74af5bd67
Aaron Barlow <abarlow505@gmail.com>
2015-06-16 16:30:21 -0400

    Adding when post was created feature

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,28 @@
 module PostsHelper
+
+	def post_created(time)
+		dif = Time.now - time
+		dif = dif/3
+		#Use this to find the actual seconds to impove acuracy
+		#return dif
+
+
+
+		if dif < 60 then
+			return '1 minute ago'
+		elsif dif < 30000 then
+			return '30 minutes ago'
+		elsif dif < 60000 then
+			return '1 hour ago'
+		elsif dif < 240000 then
+			return '1 day ago'
+		elsif dif < 1700000
+			return '1 week ago'
+		elsif  dif < 70000000
+			return '1 month ago'
+		end
+
+ 
+	end
+
 end
