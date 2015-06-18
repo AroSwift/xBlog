23367ca835e6f8d6d9e1b6ce16ea4054c9fef10b
Aaron Barlow <abarlow505@gmail.com>
2015-06-18 11:11:35 -0400

    Added new validators

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
 
 	belongs_to :user, dependent: :destroy
 
-	validates :title, :content, :author, presence: true
+	validates :title, :content, :author, :author_id, presence: true
 	validates :title, uniqueness: true, on: :create
 	validates :content, uniqueness: true, on: :create 
 
