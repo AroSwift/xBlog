35348e7b53f3bcee697396d2aa856767ab1249cb
Aaron Barlow <abarlow505@gmail.com>
2015-06-19 15:15:09 -0400

    Added length and on:create  validators for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,8 @@ class Post < ActiveRecord::Base
 	validates :title, :content, :author, :author_id, presence: true
 	validates :title, uniqueness: true, on: :create
 	validates :content, uniqueness: true, on: :create 
+	validates :title, length: { in: 5..30 }
+	validates :content, length: { in: 10..10000 } # CHANGE MIN CHARACTERS LATER
 
 end
 
