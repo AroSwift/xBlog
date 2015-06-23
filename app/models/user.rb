1e41dd2bb73ecc5511a14546f5601ae38b55a625
Aaron Barlow <abarlow505@gmail.com>
2015-06-23 12:30:42 -0400

    Added uniqueness validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 	#foreign key somewhere here or in post.rb
 	has_many :posts
 
+	validates :username, uniqueness: true
 	validates :username, :password, presence: true
 	validates :username, length: { in: 5..12 }
 	validates :password, length: { in: 8..20 }
