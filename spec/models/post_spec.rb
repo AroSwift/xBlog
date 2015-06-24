03355e0d74dddf512c804f4655e4b78f62c7f536
Aaron Barlow <abarlow505@gmail.com>
2015-06-24 12:25:38 -0400

    Added posts RSpec controller

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,2 +1,20 @@
 require 'rails_helper'
 require 'factory_girl'
+
+
+describe Post do
+	# Validates presences
+	it { should validate_presence_of(:title) }	
+	it { should validate_presence_of(:author) }	
+	it { should validate_presence_of(:content) }	
+
+	# Validates length
+	it { should validate_length_of(:title).is_at_least(5) }
+	it { should validate_length_of(:title).is_at_most(40) }
+
+	it { should validate_length_of(:author).is_at_least(5) }
+	it { should validate_length_of(:author).is_at_most(12) }
+
+	it { should validate_length_of(:content).is_at_least(10) }
+	it { should validate_length_of(:content).is_at_most(10000) }
+end
