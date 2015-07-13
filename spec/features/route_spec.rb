fbb019c405335ef4a95f17ea2fca917c914f7fc2
Aaron Barlow <abarlow505@gmail.com>
2015-07-13 16:44:00 -0400

    Added all GET route checks to route spec

--- spec/features/route_spec.rb
+++ spec/features/route_spec.rb
@@ -28,7 +28,6 @@ end
 describe 'Checking Logged In Routes' do
 
 		before :each do
-			@id = 1 # Completely valid link number
 			@user = FactoryGirl.create(:user) # Completely valid user
 			@post = FactoryGirl.create(:post)
 
@@ -41,15 +40,27 @@ describe 'Checking Logged In Routes' do
 
 
 
-		it "successfully accesses New Post page", type: :request  do
+		it "successfully accesses New Post page" do
 			visit new_post_path
 			expect(page).to have_text("Post")
 		end
 
 
-		it "successfully accesses Edit Post page", type: :request  do
-			visit edit_post_path(@id)
+		it "successfully accesses Edit Post page" do
+			visit edit_post_path(@post.id)
 			expect(page).to have_text("Edit Post")
+		end
+
+
+		it "successfully accesses Account page" do
+			visit account_user_path(@user.id)
+			expect(page).to have_text("Account")
+		end
+
+
+		it "successfully accesses Edit User page" do
+			visit edit_user_path(@user.id)
+			expect(page).to have_text("Edit User")
 		end
 
 end
@@ -57,5 +68,33 @@ end
 
 
 describe 'Checking Admin Routes' do
+
+
+		before :each do
+			@id = 1 # Completely valid link number
+			@user = FactoryGirl.create(:user) # Completely valid user
+			# @post = FactoryGirl.create(:post)
+
+			# Set Cookies
+			visit :login
+			fill_in "login_username", with: @user.username
+			fill_in "login_password", with: @user.password
+			click_button "Login"
+		end
+
+
+
+		it "successfully accesses Users page" do
+			visit :admin_users
+			expect(page).to have_text("Users")
+		end
+
+
+		it "successfully accesses Admin Home page" do
+			visit :admin_home
+			expect(page).to have_text("Home")
+		end
+
+
 end
 
